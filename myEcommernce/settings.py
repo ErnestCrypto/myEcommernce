6256891adf451d82507d286554d47a647a523098
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-11-24 13:41:19 +0000

    commit

--- myEcommernce/settings.py
+++ myEcommernce/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mysite.apps.MysiteConfig',
-    'dashboard.apps.DashboardConfig',
     'rest_framework',
 ]
 
